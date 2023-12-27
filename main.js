4c8841f8524e70d781db65c0d1d40a7a718729ec
timinemcv <mtiminem@gmail.com>
2023-12-26 19:55:19 -0500

    updated

--- main.js
+++ main.js
@@ -12,9 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
     nextBtn.addEventListener("click", nextPage);
 
     let currentPage = 1;
-    let numberOfPages = 3;
-    let maxPages = numberOfPages + 1;
+    let numberOfPages = 6;
+    let maxPages = numberOfPages;
 
+    function updatePageCounter() {
+        const pageCounter = document.querySelectorAll(".pageCounter");
+        pageCounter.forEach((counter) => {
+            counter.textContent = currentPage;
+        });
+        updatePageNumbers();
+    }
+    function updatePageNumbers() {
+        const pageNumbers = document.querySelectorAll(".pageNumber");
+        pageNumbers.forEach((number, index) => {
+            number.textContent = index + 1;
+        });
+    }
     function openBook() {
         mybook.style.transform = "translateX(50%)";
         prevBtn.style.transform = "translateX(-180px)";
@@ -52,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     throw new Error("unknown state");
             }
             currentPage++;
+            updatePageCounter();
         }
     }
 
@@ -76,9 +90,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     throw new Error("unknown state");
             }
             currentPage--;
+            updatePageCounter();
         }
     }
 });
+
 const bookContent = {
     title: "Learn to code",
     subTitle: "HTML & CSS",
@@ -103,79 +119,225 @@ function createFrontPageContent() {
 
     const authorHeading = document.createElement("h3");
     authorHeading.textContent = bookContent.author;
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterOne");
 
     // Append elements
     introDiv.appendChild(titleParagraph);
     introDiv.appendChild(subTitleHeading);
     introDiv.appendChild(descriptionHeading);
     introDiv.appendChild(authorHeading);
+    introDiv.appendChild(counter);
 
     const frontContent = document.getElementById("frontOne");
     frontContent.appendChild(introDiv);
 }
 
-// Call the function to create and append content to the front page
 createFrontPageContent();
-// Sample data for each page
-const pageContent = [
-    {
-        title: "Building the Foundation with HTML",
-        content: "HTML (Hypertext Markup Language) is the backbone of web development. It structures the content of a webpage and forms the foundation for creating dynamic and interactive sites. Let's explore the basics:\n\n- Tags and Elements: Understanding the building blocks.\n- Document Structure: The anatomy of an HTML document.\n- Common HTML Tags: Headings, paragraphs, lists, and more.\n- Attributes: Enhancing elements with additional information.\n\nExercises:\n1. Create a simple webpage with headings and paragraphs.\n2. Build an unordered list showcasing your favorite hobbies.\n\nRemember, HTML is the canvas on which we paint our digital creations. Let's get hands-on!",
-        pageNumber: 2
-    },
-    {
-        title: "Crafting Aesthetics with CSS",
-        content: "CSS (Cascading Style Sheets) is the artistic touch that transforms a webpage's the world of styling, understanding how to make your websites visually appealing.\n\nTopics covered:\n- Selectors and properties\n- Box model: Understanding the structure of elements\n- Colors and fonts: Adding personality to your design\n- Layouts: Positioning elements on the page\n\nGet ready to bring your websites to life with the magic of CSS!",
-        pageNumber: 3
-    },
-    {
-        title: "Harmony in Coding - HTML & CSS Integration",
-        content: "Now that we understand HTML and CSS individually, it's time to bring them together. In this chapter, we'll explore how these two languages harmonize to create well-designed and structured web pages.\n\nTopics covered:\n- Linking CSS to HTML\n- Creating a cohesive design system\n- Responsive design: Ensuring your website looks great on all devices\n- Best practices for HTML and CSS integration\n\nBy the end of this chapter, you'll be able to seamlessly integrate HTML and CSS for a polished web experience.",
-        pageNumber: 4
-    },
-    {
-        title: "Adapting to Every Screen - Responsive Web Design",
-        content: "In the age of smartphones and tablets, it's crucial to create websites that adapt to various screen sizes. In this chapter, we'll delve into the principles of responsive web design.\n\nTopics covered:\n- Media queries: Tailoring styles for different devices\n- Flexbox and Grid: Powerful layout tools\n- Mobile-first design approach\n- Testing and optimizing for responsiveness\n\nEquip yourself with the skills to create websites that look fantastic on any device!",
-        pageNumber: 5
-    },
-    {
-        title: "Journey into Advanced Concepts",
-        content: "Congratulations on reaching the final chapter! In this section, we'll explore advanced concepts and tools that will take your coding skills to the next level.\n\nTopics covered:\n- CSS preprocessors (Sass, Less)\n- Version control with Git\n- Introduction to JavaScript\n- Tools and frameworks for web development\n\nThis chapter marks the beginning of your journey beyond the basics. Embrace the continuous learning mindset, and remember that coding is an ever-evolving adventure.\n\nThank you for joining us on this exciting exploration of coding with HTML and CSS. Happy coding!",
-        pageNumber: 6
-    }
-];
 
-// Function to create and append content to the specified page
-function createPageContent(pageData) {
-    const frontContentElement = document.getElementById(`front${pageData.pageNumber}`);
-    const backContentElement = document.getElementById(`back${pageData.pageNumber}`);
+const firstPageContent = {
+    topHeader: "Building the Foundation with HTML",
+    paraGraph: `HTML (Hypertext Markup Language) is the backbone of web
+    development. It structures the content of a webpage and
+    forms the foundation for creating dynamic and interactive
+    sites. Let's explore the basics:
+    
+    - Tags and Elements: Understanding the building blocks.
+    - Document Structure: The anatomy of an HTML document.
+    - Common HTML Tags: Headings, paragraphs, lists, and more.
+    - Attributes: Enhancing elements with additional information.
+    
+    Exercises:
+    1. Create a simple webpage with headings and paragraphs.
+    2. Build an unordered list showcasing your favorite hobbies.
+    
+    Remember, HTML is the canvas on which we paint our digital creations. Let's get hands-on!`,
+};
 
-    // Create elements
-    const pageHeading = document.createElement("h3");
-    pageHeading.textContent = pageData.title;
+function createFirstPageContent() {
+    const pageContent = document.createElement("div");
+    pageContent.classList.add("pageContentOne");
 
-    const pageParagraph = document.createElement("p");
-    pageParagraph.innerHTML = pageData.content;
+    const titlePageOne = document.createElement("h3");
+    titlePageOne.textContent = firstPageContent.topHeader;
 
-    const pageNumber = document.createElement("h6");
-    pageNumber.textContent = pageData.pageNumber;
+    const mainContent = document.createElement("p");
+    mainContent.textContent = firstPageContent.paraGraph;
 
-    // Append elements
-    const pageContentDiv = document.createElement("div");
-    pageContentDiv.classList.add("pageContentOne", `pageCont${pageData.pageNumber}`);
-    pageContentDiv.appendChild(pageHeading);
-    pageContentDiv.appendChild(pageParagraph);
-    pageContentDiv.appendChild(pageNumber);
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterTwo");
 
-    if (frontContentElement) {
-        frontContentElement.appendChild(pageContentDiv);
-    }
+    pageContent.appendChild(titlePageOne);
+    pageContent.appendChild(mainContent);
+    pageContent.appendChild(counter);
 
-    if (backContentElement) {
-        backContentElement.innerHTML = ""; // Clear existing content
-        backContentElement.appendChild(pageContentDiv);
-    }
+    const backContent = document.getElementById("backOne");
+    backContent.appendChild(pageContent);
 }
+createFirstPageContent();
 
-// Call the function for each page
-pageContent.forEach(createPageContent);
+const secondPageContent = {
+    topHeaderTwo: "Crafting Aesthetics with CSSL",
+    pageContentTwo: `CSS (Cascading Style Sheets) is the artistic
+    touch that transforms a webpage's
+    the world of styling, understanding how to make your websites visually appealing.
+    
+    Topics covered:
+    - Selectors and properties
+    - Box model: Understanding the structure of elements
+    - Colors and fonts: Adding personality to your design
+    - Layouts: Positioning elements on the page
+    
+    Get ready to bring your websites to life with the magic of CSS!`,
+};
+
+function createSecondContent() {
+    const pageContentTwo = document.createElement("div");
+    pageContentTwo.classList.add("pageContentOne");
+
+    const pageContentHeader = document.createElement("h3");
+    pageContentHeader.textContent = secondPageContent.topHeaderTwo;
+
+    const mainContentTwo = document.createElement("p");
+    mainContentTwo.textContent = secondPageContent.pageContentTwo;
+
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterThree");
+
+    pageContentTwo.appendChild(pageContentHeader);
+    pageContentTwo.appendChild(mainContentTwo);
+    pageContentTwo.appendChild(counter);
+
+    const frontTwo = document.getElementById("frontTwo");
+    frontTwo.appendChild(pageContentTwo);
+}
+createSecondContent();
+
+const thirdPageContent = {
+    topHeaderThree: "Harmony in Coding - HTML & CSS Integration",
+    pageContentThree: `Now that we understand HTML and CSS individually, it's
+    time to bring them together. In this chapter,
+    we'll explore how these two languages harmonize to create
+    well-designed and structured web pages.
+    
+    Topics covered:
+    - Linking CSS to HTML
+    - Creating a cohesive design system
+    - Responsive design: Ensuring your website looks great on
+    all devices
+    - Best practices for HTML and CSS integration
+    
+    By the end of this chapter, you'll be able to seamlessly
+    integrate HTML and CSS for a polished web experience.`,
+};
+
+function createThirdContent() {
+    const pageContentThree = document.createElement("div");
+    pageContentThree.classList.add("pageContentOne");
+
+    const pageContentHeader = document.createElement("h3");
+    pageContentHeader.textContent = thirdPageContent.topHeaderThree;
+
+    const mainContentThree = document.createElement("p");
+    mainContentThree.textContent = thirdPageContent.pageContentThree;
+
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterFour");
+
+    pageContentThree.appendChild(pageContentHeader);
+    pageContentThree.appendChild(mainContentThree);
+    pageContentThree.appendChild(counter);
+
+    const backThree = document.getElementById("backTwo");
+    backThree.appendChild(pageContentThree);
+}
+createThirdContent();
+
+const fourthpageContent = {
+    topHeaderFour: "Adapting to Every Screen - Responsive Web Design",
+    pageContentFour: `In the age of smartphones and tablets, it's crucial to
+    create websites that adapt to various screen sizes. In
+    this chapter, we'll delve into the principles of
+    responsive web design.
+    
+    Topics covered:
+    - Media queries: Tailoring styles for different devices
+    - Flexbox and Grid: Powerful layout tools
+    - Mobile-first design approach
+    - Testing and optimizing for responsiveness
+    
+    Equip yourself with the skills to create websites that
+    look fantastic on any device!
+    `,
+};
+
+function createFourthPage() {
+    const pageContentFour = document.createElement("div");
+    pageContentFour.classList.add("pageContentOne");
+
+    const pageContentHeader = document.createElement("h3");
+    pageContentHeader.textContent = fourthpageContent.topHeaderFour;
+
+    const mainContentFour = document.createElement("p");
+    mainContentFour.textContent = fourthpageContent.pageContentFour;
+
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterFive");
+
+    pageContentFour.appendChild(pageContentHeader);
+    pageContentFour.appendChild(mainContentFour);
+    pageContentFour.appendChild(counter);
+
+    const frontThree = document.getElementById("frontThree");
+    frontThree.appendChild(pageContentFour);
+}
+createFourthPage();
+
+const lastPageContent = {
+    lastPageHeader: "Journey into Advanced Concepts",
+    pageContentLast: `Congratulations on reaching the final chapter! In this
+    section, we'll explore advanced
+    concepts and tools that will take your coding skills
+    to the next level.
+    
+    Topics covered:
+    - CSS preprocessors (Sass, Less)
+    - Version control with Git - Introduction to JavaScript
+    - Tools and frameworks for web development
+    
+    This chapter marks the beginning of your
+    journey beyond the basics. Embrace the
+    continuous learning mindset, and
+    remember that coding is an ever-evolving adventure.
+    
+    Thank you for joining us on this exciting exploration of
+    coding with HTML and CSS. Happy coding!`,
+};
+
+function createLastPage() {
+    const pageContentLast = document.createElement("div");
+    pageContentLast.classList.add("pageContentOne");
+
+    const pageContentHeader = document.createElement("h3");
+    pageContentHeader.textContent = lastPageContent.lastPageHeader;
+
+    const mainContentFour = document.createElement("p");
+    mainContentFour.textContent = lastPageContent.pageContentLast;
+
+    const counter = document.createElement("h6");
+    counter.classList.add("pageCounter");
+    counter.setAttribute("id", "pageCounterSix");
+
+    pageContentLast.appendChild(pageContentHeader);
+    pageContentLast.appendChild(mainContentFour);
+    pageContentLast.appendChild(counter);
+
+    const lastPage = document.getElementById("backThree");
+    lastPage.appendChild(pageContentLast);
+}
+createLastPage();
